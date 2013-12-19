22289f87a2abd9bfd1aed4b2ec627287da23c7e8
Jan Tusil <jenda.tusil@gmail.com>
2013-12-19 11:36:27 +0100

    homeworks

--- 2013_fall/pv198/hw/hw01/Solution/main.c
+++ 2013_fall/pv198/hw/hw01/Solution/main.c
@@ -13,21 +13,150 @@
 * Target system : Charon 2 Development Kit                                *
 *                 ATmega128 14,7456 MHz, UART: 9600,N,8,1                 *
 \*************************************************************************/
-#include "shift.h"
+#include <stdlib.h>
+#include "config.h"
+
+#define nop() asm volatile("nop\n\t")
 
 void DevBoardShiftLedOut( uint8_t val );
 uint8_t DevBoardShiftByteIn( void );
+
+
+typedef struct {
+	void (*init)(void);
+	void (*fun)(void);
+} functionality_t;
+
+union {
+	struct {
+		uint8_t cnt;
+		uint8_t dir;
+	} f_0;
+
+	struct {
+		uint8_t cnt;
+		uint8_t dir;
+	} f_1;
+
+	struct {
+		uint8_t i;
+	} f_2;
+
+	struct {
+
+	} f_3;
+} u;
+
+void f0_bouncing_led_init(void)
+{
+	u.f_0.cnt = 0;
+	u.f_0.dir = 0;
+}
+
+
+void f0_bouncing_led_step(void)
+{
+	DevBoardShiftLedOut(~(1 << u.f_0.cnt));
+	if (u.f_0.dir == 1) {
+		if (u.f_0.cnt != 0) {
+			u.f_0.cnt--;
+		} else {
+			u.f_0.dir = 0;
+		}
+	} else {
+		if (u.f_0.cnt != 7) {
+			u.f_0.cnt++;
+		} else {
+			u.f_0.dir = 1;
+		}
+	}
+}
+
+
+void f1_bouncing_2leds_init(void)
+{
+	u.f_1.cnt = 0;
+	u.f_1.dir = 0;
+}
+
+
+void f1_bouncing_2leds_step(void)
+{
+	DevBoardShiftLedOut(1 << u.f_1.cnt | 0x80 >> u.f_1.cnt);
+	if (u.f_1.dir == 1) {
+		if (u.f_1.cnt != 0) {
+			u.f_1.cnt--;
+		} else {
+			u.f_0.dir = 0;
+		}
+	} else {
+		if (u.f_1.cnt != 3) {
+			u.f_1.cnt++;
+		} else {
+			u.f_1.dir = 1;
+		}
+	}
+}
+
+void f2_strange_led_init(void)
+{
+	u.f_2.i = 0;
+}
+
+
+void f2_strange_led_step(void)
+{
+	static const char arr[] = {0x01,0x80,0x44,0x12};
+	DevBoardShiftLedOut(~(arr[u.f_2.i++]));
+	u.f_2.i %= 4;
+}
+
+
+
+
+static functionality_t handlers[4] = {
+	{
+		.init = f0_bouncing_led_init,
+		.fun = f0_bouncing_led_step
+	},
+	{
+		.init = f1_bouncing_2leds_init,
+		.fun = f1_bouncing_2leds_step
+	},
+	{
+		.init = f2_strange_led_init,
+		.fun = f2_strange_led_step
+	
+	},
+	{
+		.init = NULL,
+		.fun = NULL
+	}
+};
+
 
 /*
  * MAIN
  */
 int main(void)
 {
+	uint8_t state = 0xFF;
 	while(1)
 	{
-		uint8_t rv;
-		/* Rewrite inputs from DIP SWITCH to bargraph */
-		DevBoardShiftLedOut(DevBoardShiftByteIn());
+		uint8_t new_state;
+
+		new_state = 0x03 & DevBoardShiftByteIn();
+		if (new_state != state) {
+			if (handlers[new_state].init)
+				handlers[new_state].init();
+			state = new_state;
+		}
+
+		if (handlers[state].fun)
+			handlers[state].fun();
+
+
+		// TODO: insert a delay here
 	}
 }
 
@@ -52,11 +181,11 @@ void DevBoardShiftLedOut( uint8_t val )
 
 		SHIFT_PORT |= _BV(SHIFT_CLK);   /* 0 -> 1 data valid */
 
-		asm volatile("nop\n\t");	/* wait */
+		nop();
 		SHIFT_PORT &= ~(_BV(SHIFT_CLK));
 	}
 
-	asm volatile("nop\n\t");		/* wait */
+	nop();
 	SHIFT_PORT |= _BV(SHIFT_LED_SET);	/* 0 -> 1 parallel write */
 }
 
@@ -74,7 +203,7 @@ uint8_t DevBoardShiftByteIn( void )
 	SHIFT_PORT &= ~(_BV(SHIFT_CLK));	/* set clock low  */
 	SHIFT_PORT &= ~(_BV(SHIFT_IN_LOAD));	/* set load low  -> parallel in */
 
-	asm volatile("nop\n\t");		/* wait */
+	nop();
 	SHIFT_PORT |= _BV(SHIFT_IN_LOAD);       /* set load high -> serial out */
 
 	/* read bit, MSB(most significant bit) first */
@@ -82,11 +211,11 @@ uint8_t DevBoardShiftByteIn( void )
 	while(i--)
 	{
 		SHIFT_PORT |= _BV(SHIFT_CLK);	/* 0 -> 1 next databit on serial output is valid */
-		asm volatile("nop\n\t");	/* wait */
+		nop();
 		in_byte <<= 1;			/* shift one bit left */
 		bit_is_set(SHIFT_PIN, SHIFT_IN) ? (in_byte |= 1) : (in_byte |= 0); /* read next bit */
 		SHIFT_PORT &= ~(_BV(SHIFT_CLK));
-		asm volatile("nop\n\t");	/* wait */
+		nop();
 	}
 
 	return(in_byte);			/* return received byte */
