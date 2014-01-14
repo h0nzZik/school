5f63070d0e2c6d2347f696e625098296cf9ece27
Jan Tusil <jenda.tusil@gmail.com>
2014-01-14 19:29:44 +0100

    HW01

--- 2013_fall/pv198/hw/hw01/Solution/main.c
+++ 2013_fall/pv198/hw/hw01/Solution/main.c
@@ -1,11 +1,11 @@
 /*************************************************************************\
-*                  S h i f t - O U T                                      *
+*                  LED Stranger                                           *
 *-------------------------------------------------------------------------*
-* Description :  The aim of this example is to be simple as possible.     *
+* Description :  The aim of this example is to be strange as possible.    *
 *                You should train how to build simple C program and how   *
 *                to program ATmega128 via JTAG or SPI interface.          *
 *-------------------------------------------------------------------------*
-* Author      : Zbynek Bures & Pavel Celeda                               *
+* Author      : Zbynek Bures & Pavel Celeda & Jan Tusil                   *
 * Version     :
 *-------------------------------------------------------------------------*
 * Compiler    : gcc                       Version:  4.1.0                 *
@@ -14,12 +14,15 @@
 *                 ATmega128 14,7456 MHz, UART: 9600,N,8,1                 *
 \*************************************************************************/
 #include <stdlib.h>
+#include <util/delay.h>
 #include "config.h"
 
 #define nop() asm volatile("nop\n\t")
 
 void DevBoardShiftLedOut( uint8_t val );
 uint8_t DevBoardShiftByteIn( void );
+
+
 
 
 typedef struct {
@@ -43,6 +46,8 @@ union {
 	} f_2;
 
 	struct {
+		uint8_t state;
+		uint8_t delay;
 
 	} f_3;
 } u;
@@ -56,7 +61,7 @@ void f0_bouncing_led_init(void)
 
 void f0_bouncing_led_step(void)
 {
-	DevBoardShiftLedOut(~(1 << u.f_0.cnt));
+	PORTD = ~(1 << u.f_0.cnt);
 	if (u.f_0.dir == 1) {
 		if (u.f_0.cnt != 0) {
 			u.f_0.cnt--;
@@ -82,7 +87,7 @@ void f1_bouncing_2leds_init(void)
 
 void f1_bouncing_2leds_step(void)
 {
-	DevBoardShiftLedOut(1 << u.f_1.cnt | 0x80 >> u.f_1.cnt);
+	PORTD = 1 << u.f_1.cnt | 0x80 >> u.f_1.cnt;
 	if (u.f_1.dir == 1) {
 		if (u.f_1.cnt != 0) {
 			u.f_1.cnt--;
@@ -107,12 +112,39 @@ void f2_strange_led_init(void)
 void f2_strange_led_step(void)
 {
 	static const char arr[] = {0x01,0x80,0x44,0x12};
-	DevBoardShiftLedOut(~(arr[u.f_2.i++]));
+	PORTD = ~(arr[u.f_2.i++]);
 	u.f_2.i %= 4;
 }
 
 
+void f3_strange_led_init(void)
+{
+	u.f_3.state = 0;
+	u.f_3.delay = 0;
+}
 
+void f3_strange_led_step(void)
+{
+	u.f_3.delay++;
+	u.f_3.delay %= 10;
+	if (u.f_3.delay)
+		return;
+
+	uint8_t data = 1 << (u.f_3.state & 0x07);
+	if (u.f_3.state & 0x80)
+	{
+		data = ~data;
+		u.f_3.state &= ~0x80;
+		u.f_3.state++;
+		if (u.f_3.state >= 8)
+			u.f_3.state = 0;
+	} else {
+		u.f_3.state |= 0x80;
+	}
+
+
+	PORTD = data;
+}
 
 static functionality_t handlers[4] = {
 	{
@@ -129,8 +161,8 @@ static functionality_t handlers[4] = {
 	
 	},
 	{
-		.init = NULL,
-		.fun = NULL
+		.init = f3_strange_led_init,
+		.fun = f3_strange_led_step
 	}
 };
 
@@ -141,11 +173,13 @@ static functionality_t handlers[4] = {
 int main(void)
 {
 	uint8_t state = 0xFF;
+	DDRD  = 0xff;		/* use all pins on Port D for output */
+	DDRB  = 0x00;		/* use all pins on Port B for input */
 	while(1)
 	{
 		uint8_t new_state;
 
-		new_state = 0x03 & DevBoardShiftByteIn();
+		new_state = 0x03 & (PINB >> 6);
 		if (new_state != state) {
 			if (handlers[new_state].init)
 				handlers[new_state].init();
@@ -156,68 +190,7 @@ int main(void)
 			handlers[state].fun();
 
 
-		// TODO: insert a delay here
+		_delay_ms(80);
 	}
-}
-
-/*
- * Send data to 8x shift LED shift interface.
- */
-void DevBoardShiftLedOut( uint8_t val )
-{
-	uint8_t i=8;
-
-	SHIFT_DDR |= _BV(SHIFT_LED_SET_DDR);	/* parallel write -> output pin */
-	SHIFT_DDR |= _BV(SHIFT_CLK_DDR);	/* serial clock -> output pin */
-	SHIFT_DDR |= _BV(SHIFT_OUT_DDR);	/* serial data stream -> output pin */
-
-	SHIFT_PORT &= ~(_BV(SHIFT_LED_SET));	/* define start values */
-	SHIFT_PORT &= ~(_BV(SHIFT_CLK));
-
-	while( i-- )
-	{	 /* send data to 74595, MSB (most significant bit) first */
-		if( val & (1 << i) ) SHIFT_PORT |= _BV(SHIFT_OUT);
-		else SHIFT_PORT &= ~(_BV(SHIFT_OUT));
-
-		SHIFT_PORT |= _BV(SHIFT_CLK);   /* 0 -> 1 data valid */
-
-		nop();
-		SHIFT_PORT &= ~(_BV(SHIFT_CLK));
-	}
-
-	nop();
-	SHIFT_PORT |= _BV(SHIFT_LED_SET);	/* 0 -> 1 parallel write */
-}
-
-/*
- * Read data from 8x shift input interface.
- */
-uint8_t DevBoardShiftByteIn( void )
-{
-	uint8_t in_byte=0, i=7;
-
-	SHIFT_DDR |= _BV(SHIFT_IN_LOAD_DDR);	/* shift/!load set as output pin */
-	SHIFT_DDR |= _BV(SHIFT_CLK_DDR);	/* clock set as output pin */
-	SHIFT_DDR &= ~(_BV(SHIFT_IN_DDR));	/* serial data set as input pin */
-
-	SHIFT_PORT &= ~(_BV(SHIFT_CLK));	/* set clock low  */
-	SHIFT_PORT &= ~(_BV(SHIFT_IN_LOAD));	/* set load low  -> parallel in */
-
-	nop();
-	SHIFT_PORT |= _BV(SHIFT_IN_LOAD);       /* set load high -> serial out */
-
-	/* read bit, MSB(most significant bit) first */
-	bit_is_set(SHIFT_PIN, SHIFT_IN) ? (in_byte |= 1) : (in_byte |= 0);
-	while(i--)
-	{
-		SHIFT_PORT |= _BV(SHIFT_CLK);	/* 0 -> 1 next databit on serial output is valid */
-		nop();
-		in_byte <<= 1;			/* shift one bit left */
-		bit_is_set(SHIFT_PIN, SHIFT_IN) ? (in_byte |= 1) : (in_byte |= 0); /* read next bit */
-		SHIFT_PORT &= ~(_BV(SHIFT_CLK));
-		nop();
-	}
-
-	return(in_byte);			/* return received byte */
 }
 
